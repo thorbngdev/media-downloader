c430d45bf57a78ddda8f658d3a5aa365318a7fca
eduardogalvao <eduardo.galvao@deal.com.br>
2022-01-26 18:24:34 -0300

    Subindo o back-end do projeto

---
+++ md-service/src/main/java/com/thor/mediadownloader/MediaDownloaderApiApplication.java
@@ -0,0 +1,13 @@
+package com.thor.mediadownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaDownloaderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaDownloaderApiApplication.class, args);
+	}
+
+}
