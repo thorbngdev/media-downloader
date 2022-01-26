efe1b178bca07c40b8ea1a8c3ce3ebeabe316795
eduardogalvao <eduardo.galvao@deal.com.br>
2022-01-26 18:28:02 -0300

    Subindo back-end do projeto

---
+++ md-service/src/test/java/com/thor/mediadownloader/MediaDownloaderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thor.mediadownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaDownloaderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
