82eb8ba00bfbdd5aecf2adaf3039319b1e1f97a4
thorbngdev <eduardotorresgalvao@gmail.com>
2022-01-31 17:37:32 -0300

    Testando meta http

--- md-view/src/app/service/download-service.ts
+++ md-view/src/app/service/download-service.ts
@@ -5,18 +5,25 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class DownloadService {
 
-    constructor(private httpClient: HttpClient) {}
+    headers = new HttpHeaders();
+
+    constructor(private httpClient: HttpClient) {
+        this.headers.append('Content-Type', 'application/json');
+        // this.headers.append('Content-Security-Policy', 'upgrade-insecure-requests');
+    }
 
     getVideoInfo(url: string) {
-        return this.httpClient.get(`${environment.apiUrl}/video-info?url=${url}`);
+        return this.httpClient.get(`${environment.apiUrl}/video-info?url=${url}`,
+        {
+            headers: this.headers
+        });
     }
 
     download(downloadUrl: string, format: string) {
-        let headers = new HttpHeaders();
-        headers.append('Content-Type', 'application/json');
+        
         return this.httpClient.get(`${environment.apiUrl}/download/${format}?url=${downloadUrl}`,
         {
-            headers: headers, 
+            headers: this.headers, 
             responseType: 'blob', 
             reportProgress: true
         });
